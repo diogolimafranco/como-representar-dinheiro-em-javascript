8174ed2962e5cc684e65f06143ba414a6455202d
Diogo Franco <franco.diogolima@gmail.com>
2020-03-27 18:49:17 -0300

    Script utilizado no vídeo do youtube

---
+++ index.js
@@ -0,0 +1,22 @@
+function formatMoney(amount, decimalCount = 2, decimal = ",", thousands = ".") {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+    const negativeSign = amount < 0 ? "-" : "";
+
+    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+    let j = (i.length > 3) ? i.length % 3 : 0;
+    1.999
+
+    19999.99
+    let formatted = negativeSign;
+    formatted += (j ? i.substr(0, j) + thousands : '');
+    formatted += i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands);
+    formatted += (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+
+    return formatted
+  } catch (e) {
+    console.log(e)
+  }
+};
